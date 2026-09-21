cc4cd59259fe3d517374013a41fb511593accf71
Shaun Keys <mariobuddy@gmail.com>
2026-09-21 05:38:28 +0000

    Document postrenderer & change time attribute

--- cmd/webserver/ht/assets/postrenderer.js
+++ cmd/webserver/ht/assets/postrenderer.js
@@ -1,7 +1,26 @@
+/*
+
+# postrenderer.js
+
+This project generally tries to avoid JavaScript to make the site as accessible
+& light as possible (it even runs in lynx!). However, there are some
+quality-of-life improvements that are not possible with HTML alone. This file
+adds miscellaneous scripts to add these quality-of-life improvements as
+optional add-ons.
+
+
+## Date Localization
+
+The server should always format dates in its local timezone. However, it can
+also provide a UNIX timestamp in a `data-unixtime` attribute to have it
+converted to the user's local timezone after page load.
+
+*/
+
 window.addEventListener("load", () => {
-  const timeElements = document.querySelectorAll("[js-localtime]");
+  const timeElements = document.querySelectorAll("[data-unixtime]");
   timeElements.forEach((element) => {
-    const timestamp = element.getAttribute("js-localtime");
+    const timestamp = element.getAttribute("data-unixtime");
     if (timestamp === "null") return;
     const date = new Date(Number(timestamp));
     element.innerText = date
